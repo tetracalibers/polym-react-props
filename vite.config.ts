74607966b324e690db71c37f3412382d53eb201b
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-22 10:27:40 +0900

    eslint/prettierの導入とかライブラリビルドsetupとか

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  esbuild: {
+    jsxInject: `import React from 'react'`
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: '@polym/props',
+      fileName: (format) => `polym-props.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom']
+    }
+  }
 })
