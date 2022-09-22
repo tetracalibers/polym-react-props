5cd672079d882f7e39856a8ded3509992acd1645
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-22 10:53:27 +0900

    .gitignore反映

--- types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { PolymorphicComponentPropWithRef, PolymorphicRef } from './polymorphic/index';
-import type { PolymorphicComponentProp } from './polymorphic';
-import { getDefaultProps } from './props-def';
-import type { getPropType, Required, NotRequired } from './props-def';
-import type { CSS } from './css-type';
-export { Required, NotRequired, getPropType, getDefaultProps, PolymorphicComponentProp, PolymorphicComponentPropWithRef, PolymorphicRef, CSS };
-//# sourceMappingURL=index.d.ts.map
